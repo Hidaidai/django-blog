4515225f7a43c666bdf149ce7311e68f41cd184a
hidaidai <1016004734@qq.com>
2017-04-10 18:24:45 +0800

    整个框架都搭建起来啦

--- blog/views.py
+++ blog/views.py
@@ -8,10 +8,12 @@ from django.http import Http404
 
 # Create your views here.
 
+#主页逻辑，显示博文列表
 def home(request):
     post_list = Article.objects.all()  #获取全部的Article对象
     return render(request, 'home.html', {'post_list' : post_list})
 
+#具体博文页面逻辑，显示博文具体内容
 def detail(request, id):
     try:
         post = Article.objects.get(id=str(id))
@@ -19,3 +21,14 @@ def detail(request, id):
         raise Http404
     return render(request, 'post.html', {'post' : post})
 
+#关于“关于”页面的
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+#关于“归档”页面的
+def archives(request) :
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list, 'error' : False})
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -6,5 +6,7 @@ from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
-    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<id>\d+)/$', views.detail, name= 'detail'),
+    url(r'^archives/$', views.archives, name = 'archives'),
+    url(r'^aboutme/$', views.about_me, name = 'about_me'),
 ]

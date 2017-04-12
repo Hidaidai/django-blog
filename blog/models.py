f357bf813428404fe56d87f03c19aac8f53d50bb
hidaidai <1016004734@qq.com>
2017-04-12 22:46:45 +0800

    “中期检查状态”

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField('类名', max_length=20)
+    created_time = models.DateTimeField('创建时间', auto_now_add=True)
+    last_modified_time = models.DateTimeField('修改时间', auto_now=True)
     def __str__(self):
         return self.name
     class Meta:
